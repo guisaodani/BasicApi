e176b1ac7fdf6b11b622f8134c9a23e56505bfa5
guisaodani <guisaodani@gmail.com>
2025-10-06 19:26:22 -0500

    the seeder is modified

--- BasicApi/BasicApi.BackEnd/Data/SeedDb.cs
+++ BasicApi/BasicApi.BackEnd/Data/SeedDb.cs
@@ -1,117 +1,72 @@
 ﻿using BasicApi.Shared.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace BasicApi.BackEnd.Data;
-
-public class SeedDb
+namespace BasicApi.BackEnd.Data
 {
-    private readonly DataContext _context;
-
-    public SeedDb(DataContext context)
+    public class SeedDb
     {
-        _context = context;
-    }
+        private readonly DataContext _context;
+        private readonly Random _random = new Random();
 
-    public async Task SeedAsync()
-    {
-        await _context.Database.EnsureCreatedAsync();
-        await CheckEmployeesAsync();
-    }
-
-    private async Task CheckEmployeesAsync()
-    {
-        if (!_context.Employees.Any())
+        public SeedDb(DataContext context)
         {
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Juan",
-                LastName = "Guisao",
-                IsActive = true,
-                HireDate = DateTime.UtcNow,
-                Salary = 2500000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Juan",
-                LastName = "Perez",
-                IsActive = true,
-                HireDate = new DateTime(2025, 1, 15),
-                Salary = 1000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Juan",
-                LastName = "Perez",
-                IsActive = true,
-                HireDate = new DateTime(2025, 6, 15),
-                Salary = 15000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Julian",
-                LastName = "Goez",
-                IsActive = true,
-                HireDate = DateTime.UtcNow,
-                Salary = 10000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Jorge",
-                LastName = "Castro",
-                IsActive = true,
-                HireDate = DateTime.UtcNow,
-                Salary = 10100000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Carolina",
-                LastName = "Gonzalez",
-                IsActive = true,
-                HireDate = DateTime.UtcNow,
-                Salary = 6000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Andrea",
-                LastName = "Cruz",
-                IsActive = false,
-                HireDate = DateTime.UtcNow,
-                Salary = 8000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Deisy",
-                LastName = "Perez",
-                IsActive = false,
-                HireDate = DateTime.UtcNow,
-                Salary = 8000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Deisy",
-                LastName = "Perez",
-                IsActive = false,
-                HireDate = DateTime.UtcNow,
-                Salary = 8000000
-            });
-
-            _context.Employees.Add(new Employee
-            {
-                FirstName = "Juan",
-                LastName = "Zuluaga",
-                IsActive = true,
-                HireDate = DateTime.UtcNow,
-                Salary = 25000000
-            });
+            _context = context;
         }
-        await _context.SaveChangesAsync();
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckEmployeesAsync();
+        }
+
+        private async Task CheckEmployeesAsync()
+        {
+            if (!_context.Employees.Any())
+            {
+                var firstNames = new[]
+                {
+                    "Juan", "Carolina", "Andrés", "Valentina", "Camilo", "Laura", "Mateo", "Sara", "Felipe", "Mariana",
+                    "Santiago", "Natalia", "Jorge", "Diana", "Alejandro", "Paula", "Daniel", "Isabella", "Samuel", "Juliana"
+                };
+
+                var lastNames = new[]
+                {
+                    "González", "Rodríguez", "López", "Martínez", "Pérez", "Gómez", "Torres", "Ramírez", "Castro", "Hernández",
+                    "Moreno", "Vargas", "Jiménez", "Cruz", "Mendoza", "Ortega", "Suárez", "Romero", "Rojas", "Zapata"
+                };
+
+                var employees = new List<Employee>();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    var employee = new Employee
+                    {
+                        FirstName = firstNames[_random.Next(firstNames.Length)],
+                        LastName = lastNames[_random.Next(lastNames.Length)],
+                        IsActive = _random.Next(0, 2) == 1, // true o false aleatorio
+                        HireDate = RandomHireDate(),
+                        Salary = RandomSalary()
+                    };
+
+                    employees.Add(employee);
+                }
+
+                _context.Employees.AddRange(employees);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private DateTime RandomHireDate()
+        {
+            int year = _random.Next(2015, DateTime.Now.Year + 1);
+            int month = _random.Next(1, 13);
+            int day = _random.Next(1, 28);
+            return new DateTime(year, month, day);
+        }
+
+        private decimal RandomSalary()
+        {
+            return _random.Next(1000000, 25000001);
+        }
     }
 }
