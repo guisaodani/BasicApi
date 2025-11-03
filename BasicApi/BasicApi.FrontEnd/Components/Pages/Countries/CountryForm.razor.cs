b7e41c175c662158c0c9b4506a221f97c3aecfaa
guisaodani <guisaodani@gmail.com>
2025-11-03 12:03:07 -0500

    created crud on contries

---
+++ BasicApi/BasicApi.FrontEnd/Components/Pages/Countries/CountryForm.razor.cs
@@ -0,0 +1,19 @@
+using BasicApi.Shared.Entities;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+
+namespace BasicApi.FrontEnd.Components.Pages.Countries;
+
+public partial class CountryForm
+{
+    private EditContext editContext = null!;
+
+    [EditorRequired, Parameter] public Country Country { get; set; } = null!;
+    [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+    [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+
+    protected override void OnInitialized()
+    {
+        editContext = new(Country);
+    }
+}
